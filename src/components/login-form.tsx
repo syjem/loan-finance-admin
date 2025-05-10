83f39cac099ff030efdf33fb7eb3b48383ca4ed2
Jemuel Repoylo <syjem143@gmail.com>
2025-05-10 22:16:39 +0800

    feat: auth & login pages

---
+++ src/components/login-form.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { createClient } from "@/lib/supabase/client";
+import { AppLogo, Github, Google } from "@/components/icons";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const [error, setError] = useState<string | null>(null);
+  const [loadingProvider, setLoadingProvider] = useState<
+    "github" | "google" | null
+  >(null);
+
+  const handleSocialLogin = async (provider: "github" | "google") => {
+    const supabase = createClient();
+    setLoadingProvider(provider);
+    setError(null);
+
+    try {
+      const { error } = await supabase.auth.signInWithOAuth({
+        provider,
+        options: {
+          redirectTo: `${window.location.origin}/auth/oauth?next=/protected`,
+        },
+      });
+
+      if (error) throw error;
+    } catch (error: unknown) {
+      setError(error instanceof Error ? error.message : "An error occurred");
+      setLoadingProvider(null);
+    }
+  };
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <form>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center gap-2">
+            <a
+              href="#"
+              className="flex flex-col items-center gap-2 font-medium"
+            >
+              <div className="flex h-8 w-8 items-center justify-center rounded-md">
+                <AppLogo />
+              </div>
+            </a>
+            <h1 className="text-xl font-bold">
+              Welcome to Loan Finance Tracker
+            </h1>
+            <div className="text-center text-sm text-muted-foreground">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </div>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+            <span className="relative z-10 bg-background px-2 text-muted-foreground">
+              Or
+            </span>
+          </div>
+          <div className="grid gap-4 sm:grid-cols-2">
+            <Button
+              type="button"
+              disabled={loadingProvider !== null}
+              variant="outline"
+              className="w-full"
+              onClick={() => handleSocialLogin("github")}
+            >
+              <Github />
+              {loadingProvider === "github"
+                ? "Logging in..."
+                : "Continue with GitHub"}
+            </Button>
+            <Button
+              type="button"
+              disabled={loadingProvider !== null}
+              variant="outline"
+              className="w-full"
+              onClick={() => handleSocialLogin("google")}
+            >
+              <Google />
+              {loadingProvider === "google"
+                ? "Logging in..."
+                : "Continue with Google"}
+            </Button>
+          </div>
+          {error && (
+            <div className="text-sm text-destructive text-center">{error}</div>
+          )}
+        </div>
+      </form>
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary  ">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  );
+}
